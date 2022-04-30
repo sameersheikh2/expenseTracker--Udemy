fac2bcfc30320d950f314187b052886b7d54a656
sameersheikh2 <sameersheikh819@gmail.com>
2022-04-30 20:10:36 +0530

    passing function using props to communicate B/W child and parent(bottom-up)

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -1,53 +1,67 @@
-import React, { useState } from 'react'
-import "./ExpenseForm.css"
-const ExpenseForm = () => {
-    const [enterdTitle, setEnterdTitle] = useState('');
-    const [enterdAmount, setEnterdAmount] = useState("");
-    const [enterdDate, setEnterdDate] = useState("");
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-    const titleChangeHandler= (e) => {
-        setEnterdTitle(e.target.value)
-    };
-    
-    const amountChangeHandler= (e) => {
-        setEnterdAmount(e.target.value)
-    };
-    
-    const dateChangeHandler= (e) => {
-        setEnterdDate(e.target.value)
-    };
+  const titleChangeHandler = (e) => {
+    setEnteredTitle(e.target.value);
+  };
 
-    const submitHandler= (e) => {
-        e.perventDefault();
+  const amountChangeHandler = (e) => {
+    setEnteredAmount(e.target.value);
+  };
 
-        const expenseData= {
-            title: enterdTitle,
-            amount: enterdAmount,
-            date: new Date(enterdDate)
-        };
+  const dateChangeHandler = (e) => {
+    setEnteredDate(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.perventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
     };
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
 
   return (
     <form onSubmit={submitHandler}>
-        <div className='new-expense__controls'>
-            <div className='new-expense__control'>
-                <label>Title</label>
-                <input type='text' onChange={titleChangeHandler} />
-            </div>
-            <div className='new-expense__control'>
-                <label>Amount</label>
-                <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
-            </div>
-            <div className='new-expense__control'>
-                <label>date</label>
-                <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler}/>
-            </div>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler} />
         </div>
-            <div className='new-expense__actions'>
-                <button type='submit' >Add submit</button>
-            </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add submit</button>
+      </div>
     </form>
-    )
-}
+  );
+};
 
 export default ExpenseForm;
