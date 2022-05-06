ee80a9bacb8245ff2c53768a7feabede1d9f1a98
sameersheikh2 <sameersheikh819@gmail.com>
2022-05-06 21:24:35 +0530

    added charts

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import ChartBar from "./ChartBar.js";
+
+function Chart() {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Chart;
